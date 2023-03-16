e084e74642eed3bfc51166e1ab5d846ecc6f6290
roddyisthebest <bsy17171@naver.com>
2023-03-16 11:21:34 +0900

    #4 퍼블리싱 기능 사용 설정 수정

--- screen/team/Setting.tsx
+++ screen/team/Setting.tsx
@@ -1,12 +1,58 @@
-import {Text, View} from 'react-native';
-import React from 'react';
+import React, {useEffect} from 'react';
+import Layout from '../../components/layout';
+import Setting from '../../components/container/Setting';
+import {GapRowView} from '../../components/basic/View';
+import {Switch} from 'react-native';
+import Header from '../../components/container/Header';
+import FastImage from 'react-native-fast-image';
+function SettingView({
+  navigation: {setOptions},
+}: {
+  navigation: {setOptions: Function};
+}) {
+  useEffect(() => {
+    setOptions({
+      headerTitle: () => (
+        <FastImage
+          source={require('../../assets/img/AppLogo512h.png')}
+          style={{width: 30, height: 30}}
+        />
+      ),
+      headerTitleAlign: 'center',
+    });
+  }, [setOptions]);
 
-function Setting() {
   return (
-    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <Text>Setting</Text>
-    </View>
+    <Layout scrollable={false} isItWhite={false}>
+      <GapRowView
+        gap={15}
+        marginBottom={0}
+        marginTop={0}
+        paddingHorizontal={0}
+        paddingVertical={0}>
+        <Header
+          title="기능 사용 설정"
+          contents="This app provides private content.
+Please enter a 6-digit code to prove yourself."
+        />
+        <Setting
+          title="매일성경"
+          contents="매일성경(큐티) 업로드를 사용할 수 있습니다. ">
+          <Switch />
+        </Setting>
+        <Setting
+          title="기도제목"
+          contents="기도제목을 업로드하고 수정 및 삭제 할 수 있습니다.">
+          <Switch />
+        </Setting>
+        <Setting
+          title="벌금"
+          contents="매일 성경(큐티) 벌금 로직에 참여할 수 있습니다.">
+          <Switch />
+        </Setting>
+      </GapRowView>
+    </Layout>
   );
 }
 
-export default Setting;
+export default SettingView;
